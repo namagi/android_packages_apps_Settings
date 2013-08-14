abfb707c1eda682eeeb4351e8e0b58d0c50ce017
Danny Baumann <dannybaumann@web.de>
2013-08-14 16:29:23 +0200

    Re-add link to DeviceParts.
    Change-Id: Ib595b81a11e19f8526e532400e57f5771db09499
    
    Conflicts:
    
    	src/com/android/settings/cyanogenmod/MoreDeviceSettings.java

--- src/com/android/settings/cyanogenmod/MoreDeviceSettings.java
+++ src/com/android/settings/cyanogenmod/MoreDeviceSettings.java
@@ -26,10 +26,13 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String TAG = "MoreDeviceSettings";
 
+    private static final String KEY_DEVICE_PARTS = "advanced_settings";
+    private static final String KEY_HEADSET_CONNECT_PLAYER = "headset_connect_player";
     private static final String KEY_SENORS_MOTORS_CATEGORY = "sensors_motors_category";
     private static final String KEY_DISPLAY_CALIBRATION_CATEGORY = "display_calibration_category";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
@@ -49,6 +52,13 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
 
         final PreferenceGroup calibrationCategory =
                 (PreferenceGroup) findPreference(KEY_DISPLAY_CALIBRATION_CATEGORY);
+
+        if (!DisplayColor.isSupported()) {
+            removePreference(KEY_DISPLAY_CALIBRATION_CATEGORY);
+        }
+
+        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
+                getPreferenceScreen(), KEY_DEVICE_PARTS);
 
         if (!DisplayColor.isSupported() && !DisplayGamma.isSupported()) {
             getPreferenceScreen().removePreference(calibrationCategory);
